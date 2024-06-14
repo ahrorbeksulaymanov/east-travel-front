04862de4e25710c3a411f835be6213e78e710bac
ahrorbeksulaymanov <ahrorsulaymanov2@gmail.com>
2024-06-14 17:10:07 +0500

    url is changed

--- congif/constans.tsx
+++ congif/constans.tsx
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://86.106.181.30:8888/api";
 // export const FILE_URL = "http://86.106.181.30:8888/api/file/";
 
-export const BASE_URL = "http://178.157.91.194:8888/api";
-export const FILE_URL = "http://178.157.91.194:8888/api/file/";
+export const BASE_URL = "http://apis.easttour.uz/api";
+export const FILE_URL = "http://apis.easttour.uz/api/file/";
